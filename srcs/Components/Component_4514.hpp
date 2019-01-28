c54fa871f091c178a0cef3490f7665982c46f704
Arthur Dassier <arthur.dassier@epitech.eu>
2019-01-28 15:12:58 +0100

    continuing

--- srcs/Components/Component_4514.hpp
+++ srcs/Components/Component_4514.hpp
@@ -13,20 +13,30 @@
 class Component_4514 : public nts::IComponent
 {
     public:
-        Component_4514();
+        Component_4514(std::string name);
         ~Component_4514();
 
         nts::Tristate compute(std::size_t pin = 1);
         void dump() const;
         void setLink(std::size_t, nts::IComponent &, std::size_t);
+        std::string getName();
+
+    private:
+        std::string _name;
 };
 
-Component_4514::Component_4514()
+Component_4514::Component_4514(std::string name) :
+    _name(name)
 {
 }
 
 Component_4514::~Component_4514()
 {
+}
+
+std::string Component_4514::getName()
+{
+    return (_name);
 }
 
 nts::Tristate Component_4514::compute(std::size_t pin = 1)
--- srcs/Components/Component_4801.hpp
+++ srcs/Components/Component_4801.hpp
@@ -13,20 +13,30 @@
 class Component_4801 : public nts::IComponent
 {
     public:
-        Component_4801();
+        Component_4801(std::string name);
         ~Component_4801();
 
         nts::Tristate compute(std::size_t pin = 1);
         void dump() const;
         void setLink(std::size_t, nts::IComponent &, std::size_t);
+        std::string getName();
+
+    private:
+        std::string _name;
 };
 
-Component_4801::Component_4801()
+Component_4801::Component_4801(std::string name) :
+    _name(name)
 {
 }
 
 Component_4801::~Component_4801()
 {
+}
+
+std::string Component_4801::getName()
+{
+    return (_name);
 }
 
 nts::Tristate Component_4801::compute(std::size_t pin = 1)
